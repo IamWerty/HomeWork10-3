adefc3fc7fffe3454f78cf0c5a90d10660337887
Григорій . <mcwertyge@gmail.com>
2025-07-02 15:26:37 +0300

    add features

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,11 @@
 ﻿using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть рядок зі словами, розділеними комами: ");
+string input = Console.ReadLine();
+
+string result = input.Replace(" ", "");
+
+Console.WriteLine("Результат без пробілів:");
+Console.WriteLine(result);
